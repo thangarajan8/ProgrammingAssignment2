08f9a3f9430816a2b3138fa8286f6feea24d68ce
thangarajan8 <thangam.rajan8@gmail.com>
2014-07-27 09:40:15 +0530

    Update cacheSolve.R
    version .1

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,7 +1,7 @@
 cacheSolve <- function(x, ...) {
   s <- x$getsolve()
   if(!is.null(s)) {
-    message("getting cached data")
+    message("getting cached data from cache")
     return(s)
   }
   data <- x$get()
